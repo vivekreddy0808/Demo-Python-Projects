9b1ca49252dcbe5b265a60d1fe0e9bf393a26cbc
vivek reddy <62931563+vivekreddy0808@users.noreply.github.com>
2022-09-25 17:36:22 +0000

    Project 02: Weather Condition

---
+++ My-Projects/Wheather.py
@@ -0,0 +1,25 @@
+# BOOLEAN
+# IF: { Withdraw Amount > balance}
+#       Does not allow to withdraw
+# ESLE:
+#
+
+print('Hello world ')
+print(" **** Wheather App **** ")
+
+print('Hello world ')
+print(" **** Wheather App **** ")
+
+weather = input ('enter the weather today: ')
+
+if weather == 'cloudy' :
+  print('🌩️')
+  
+elif weather == 'rainnig':
+  print('☔')
+  
+elif weather == 'thunderstrom':
+  print ('⛈️')
+
+else :
+  print('😎')
